647a7ff4b2d9db73595d71b7e41ec621f56585c4
sitesbyjoe <joe@sitesbyjoe.com>
2017-10-13 00:28:36 -0700

    Created SubHeader component

---
+++ src/components/common/SubHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="subheader">
+    <div class="container">
+      <h1>{{heading}}</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: ['heading']
+  }
+</script>
+
+<style lang="scss" scoped>
+  .subheader {
+    background: #272931;
+    color: #fff;
+    padding: 2rem 0;
+
+    h1 {
+      color: #fff;
+      text-align: center;
+
+      span {
+        color: #923f3f;
+      }
+    }
+  }
+</style>
