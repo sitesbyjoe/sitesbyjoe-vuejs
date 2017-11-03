6cf5ca239acc3310aacbe223c83ee2e9db26c83c
sitesbyjoe <joe@sitesbyjoe.com>
2017-11-02 23:27:47 -0700

    Formatting for portfolio detail page

--- src/components/portfolio/PortfolioDetail.vue
+++ src/components/portfolio/PortfolioDetail.vue
@@ -7,42 +7,39 @@
       
       <!-- Portfolio Detail -->
       <div class="container">
-          <div class="row">
-              <!-- Poftfolio Item -->
-              <div class="col-md-7">
-                  <div class="portfolio-image-wrap" v-for="photo in item.photos" v-if="photo.image_path !== item.photos[0].image_path">
-                      <img v-bind:src="'https://sitesbyjoe.com/' + photo.image_path" alt="image" width="100%" />
-                  </div>
-              </div>
-              <!-- End Poftfolio Item -->
+        <div class="row">
+          <!-- Poftfolio Item -->
+          <div class="col-md-7">
+            <div class="portfolio-image-wrap" v-for="photo in item.photos" v-if="photo.image_path !== item.photos[0].image_path">
+              <p><img v-bind:src="'https://sitesbyjoe.com/' + photo.image_path" alt="image" width="100%" /></p>
+            </div>
+          </div>
+          <!-- End Poftfolio Item -->
 
-              <!-- Poftfolio Content -->
-              <div class="col-md-5">
-                  <div class="portfolio-item-content">
-                      <h1 class="alt-title">{{item.name}}</h1>
-                      <div class="portfolio-item-content-spc">
-                          <h5 class="alt-title">{{item.description}}</h5>
-                          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-                          <p>Quisque rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-                      </div>
-                      <div class="portfolio-item-content-spc">
-                          <h5 class="alt-title">Project details</h5>
-                          <ul class="portfolio-item-content-detail list-none">
-                              <li><span class="text-bold">Tags:</span> {{item.tags}}</li>
-                              <li><span class="text-bold">Date Created:</span> {{item.launch_date}}</li>
-                              <li><span class="text-bold">Website:</span> {{item.url}}</li>
-                              <li><b>Technologies Used:</b> {{item.technologies}}</li>
-                              <li><b>Website Live?</b> {{item.published}}</li>
-                              <li><b>Solution:</b> {{item.solution}}</li>
-                              <li><b>Client Said:</b> {{item.comment}}</li>
-                          </ul>
-                      </div>
-                      <div class="portfolio-item-content-spc">
-                          <a v-bind:href="item.url" target="_blank" class="btn btn-black-outline btn-lg">Launch Website<i class="fa fa-external-link right"></i></a>
-                      </div>
-                  </div>
+          <!-- Poftfolio Content -->
+          <div class="col-md-5">
+            <div class="portfolio-item-content">
+              <h1 class="alt-title">{{item.name}}</h1>
+                <div class="portfolio-item-content-spc">
+                  <h5 class="alt-title">{{item.description}}</h5>
+                </div>
+                <div class="portfolio-item-content-spc">
+                  <h5 class="alt-title">Project Details</h5>
+                  <ul class="portfolio-item-content-detail list-none">
+                    <li v-if="item.tags"><span class="text-bold">Tags:</span> {{item.tags}}</li>
+                    <li v-if="item.launch_date"><span class="text-bold">Date Created:</span> {{item.launch_date}}</li>
+                    <li v-if="item.url"><span class="text-bold">Website:</span> {{item.url}}</li>
+                    <li v-if="item.technologies"><b>Technologies Used:</b> {{item.technologies}}</li>
+                    <li v-if="item.solution"><b>Solution:</b> {{item.solution}}</li>
+                    <li v-if="item.comment"><b>Client Said:</b> <i>"{{item.comment}}"</i></li>
+                  </ul>
+                </div>
+                <div class="portfolio-item-content-spc" v-if="item.published">
+                  <a v-bind:href="item.url" target="_blank" class="btn btn-black-outline btn-lg">Launch Website<i class="fa fa-external-link right"></i></a>
+                </div>
               </div>
-              <!-- End Poftfolio Content -->
+            </div>
+            <!-- End Poftfolio Content -->
           </div>
       </div>
       <!-- End Portfolio Detail -->
