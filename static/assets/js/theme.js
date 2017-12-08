02140599d4fd1e251b440ea4ba22445b325c1e6f
sitesbyjoe <joe@sitesbyjoe.com>
2017-12-08 00:37:39 -0800

    Removing functions and libraries from theme I'm not using.

--- static/assets/js/theme.js
+++ static/assets/js/theme.js
@@ -431,40 +431,7 @@ $(function () {
 
 
 
-    // ----------------------------------------------------------------
-    // lightbox Plugins (image lightbox, iframe lightbox, video lightbox)
-    // ----------------------------------------------------------------
-    function int_lightbox() {
-
-        // Portfolio Gallery Popup
-        $('.gallery-popup').magnificPopup({
-            delegate: '.gallery-popup-link',
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-            },
-            image: {
-                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                titleSrc: function (item) {
-                    return item.el.attr('title');
-                }
-            }
-        });
-
-        // Video popup (youtube, vimeo), map popup, iframe popup,
-        $('.popup-youtube, .popup-vimeo, .popup-gmaps, .video-popup').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: false,
-            fixedContentPos: false
-        });
-    };
+    
 
 
 
